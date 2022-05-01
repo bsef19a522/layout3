7f52a293e2c1a3f4fcf70189d5a4305b3b7fd5fc
bsef19a522 <bsef19a522@pucit.edu.pk>
2022-05-01 21:50:15 +0500

    Successful

---
+++ app/src/main/java/com/example/layout4/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.layout4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button btnLogin;
+
+    String correct_username = "Admin";
+    String correct_password = "Password";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.txt1);
+        password = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.button2);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (TextUtils.isEmpty(username.getText().toString()) || TextUtils.isEmpty(password.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "Empty, provide data", Toast.LENGTH_LONG).show();
+                } else if (username.getText().toString().equals(correct_username)) {
+                    //check password
+                    if (password.getText().toString().equals(correct_password)) {
+                        Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Invalid login", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Invalid Username", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        }
+}
